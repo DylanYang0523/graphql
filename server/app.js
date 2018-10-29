7e8cdad2b0570082a292cff33dd4f2cb5a312d5c
Dylan Yang <dogi9527@hotmail.com>
2018-10-29 23:26:24 +0800

    feat: #7 #8

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 
-const gqSchema = {
-
-};
-
-app.use('/graphql', graphqlHttp(gqSchema));
+app.use('/graphql', graphqlHttp({
+  schema
+}));
 
 app.listen(4000, () => {
   console.log('listening for requests on prot 4000...');
